c268fc7864a4e23205fc28f279d70f365c26e613
Michael-Mbaya <gikundamike@gmail.com>
2020-07-22 16:15:22 +0300

    append inputs elswhere

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
+// $(document).ready(function() {
+//     $("#blanks form").submit(function() {
+//         $(".person1").append("blah blah");
+//         $(".person2").append("blah blah");
+//         $(".animal").append("blah blah");
+//         $(".exclamation").append("blah blah");
+//         $(".verb").append("blah blah");
+//         $(".noun").append("blah blah");
+
+//         $("#story").show();
+
+//         event.preventDefault();
+//     });
+// });
+
+        //updated code above to the one below
+        //append() the inputs in the span classes of the story
+
 $(document).ready(function() {
-    $("#blanks form").submit(function() {
-        $(".person1").append("blah blah");
-        $(".person2").append("blah blah");
-        $(".animal").append("blah blah");
-        $(".exclamation").append("blah blah");
-        $(".verb").append("blah blah");
-        $(".noun").append("blah blah");
-
-        $("#story").show();
-
-        event.preventDefault();
-    });
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+        //below the values of vars above(form-inputs) appended below
+      $(".person1").append(person1Input);
+      $(".person2").append(person2Input);
+      $(".animal").append(animalInput);
+      $(".exclamation").append(exclamationInput);
+      $(".verb").append(verbInput);
+      $(".noun").append(nounInput);
+        
+      $("#story").show();
+        
+      event.preventDefault();   //prevent whole page refresh on submit
+            });
 });
